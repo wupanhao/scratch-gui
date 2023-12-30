deb4bdd57e2370507de19af92ee731dd7506f971
Muffin <muffin@mailbox.org>
2023-12-30 01:00:03 -0600

    merge-upstream: restore orange color for $extensions-*

--- src/lib/themes/tw/index.js
+++ src/lib/themes/tw/index.js
@@ -10,6 +10,11 @@ const lightGuiColors = {
     'looks-light-transparent': '#ff4d4d26',
     'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
 
+    'extensions-primary': 'hsla(10, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(10, 57%, 85%, 1)',
+
     'drop-highlight': '#ff8c8c',
 
     'menu-bar-background': '#ff4c4c'
@@ -48,6 +53,11 @@ const darkGuiColors = {
     'looks-transparent': '#ff4d4d59',
     'looks-light-transparent': '#ff4d4d26',
     'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
+
+    'extensions-primary': 'hsla(10, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(10, 57%, 85%, 1)',
 
     'drop-highlight': '#ff8c8c'
 };
