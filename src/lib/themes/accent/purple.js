eb4c38b46cc691e648f1ef1ca28064918be6ecc7
Muffin <muffin@mailbox.org>
2024-01-11 03:18:44 -0600

    Make purple theme completely purple

--- src/lib/themes/accent/purple.js
+++ src/lib/themes/accent/purple.js
@@ -1,4 +1,8 @@
 const guiColors = {
+    'motion-primary': 'hsla(260, 60%, 60%, 1)',
+    'motion-primary-transparent': 'hsla(260, 60%, 60%, 0.9)',
+    'motion-tertiary': 'hsla(260, 42%, 51%, 1)',
+
     'looks-secondary': 'hsla(260, 60%, 60%, 1)',
     'looks-transparent': 'hsla(260, 60%, 60%, 0.35)',
     'looks-light-transparent': 'hsla(260, 60%, 60%, 0.15)',
