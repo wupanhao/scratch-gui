50e35af556bb9bb0fbd41f26044926e0d923e33b
Muffin <muffin@mailbox.org>
2024-02-03 01:31:32 -0600

    Allow more easily overriding default project
    @sfederici

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -1,12 +1,22 @@
 import projectData from './project-data';
 
 /* eslint-disable import/no-unresolved */
+import overrideDefaultProject from '!arraybuffer-loader!./override-default-project.sb3';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
 import costume1 from '!raw-loader!./dango.svg';
 /* eslint-enable import/no-unresolved */
 import {TextEncoder} from '../tw-text-encoder';
 
 const defaultProject = translator => {
+    if (overrideDefaultProject.byteLength > 0) {
+        return [{
+            id: 0,
+            assetType: 'Project',
+            dataFormat: 'JSON',
+            data: overrideDefaultProject
+        }];
+    }
+
     let _TextEncoder;
     if (typeof TextEncoder === 'undefined') {
         _TextEncoder = require('text-encoding').TextEncoder;
