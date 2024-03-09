c7d905b1de6317ab3c8cd53ec3d540c5fba3ea0e
veggiecan0419 <137440114+veggiecan0419@users.noreply.github.com>
2024-03-09 09:08:10 +0300

    Disable host check (#906)

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ const base = {
     devServer: {
         contentBase: path.resolve(__dirname, 'build'),
         host: '0.0.0.0',
+        disableHostCheck: true,
         compress: true,
         port: process.env.PORT || 8601,
         // allows ROUTING_STYLE=wildcard to work properly
